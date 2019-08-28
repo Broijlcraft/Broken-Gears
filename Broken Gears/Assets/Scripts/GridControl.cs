5f99f09059316202b05b86eea79ffb1be0a63937
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2019-08-28 12:44:51 +0200

    push

--- Broken Gears/Assets/Scripts/GridControl.cs
+++ Broken Gears/Assets/Scripts/GridControl.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Banaan : MonoBehaviour
+public class GridControl : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
