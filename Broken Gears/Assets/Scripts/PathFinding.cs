70c6c33579999c90e7aad4be623766cf1f4c8e14
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2019-09-09 16:33:33 +0200

    p

---
+++ Broken Gears/Assets/Scripts/PathFinding.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinding : MonoBehaviour {
+
+    GameObject startPos;
+    GameObject endPos;
+    GameObject[] waypoints;
+
+    private void Start() {
+        startPos = GameObject.Find("StartPoint");
+        endPos = GameObject.Find("EndPoint");
+        waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+    }
+}
