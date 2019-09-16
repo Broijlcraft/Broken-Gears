10a4be828f668d407edda09124cfe038fed720f3
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2019-09-16 15:32:53 +0200

    a

--- Broken Gears/Assets/Scripts/Enemy.cs
+++ Broken Gears/Assets/Scripts/Enemy.cs
@@ -6,4 +6,7 @@ public class Enemy : MonoBehaviour {
 
 
 
+    public void DIE() {
+
+    }
 }
--- Broken Gears/Assets/Scripts/Health.cs
+++ Broken Gears/Assets/Scripts/Health.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Health : MonoBehaviour {
 
     public int maxHealth;
-    public int currentHealth;
+    int currentHealth;
+
+    public GameObject healthPrefab;
+    GameObject g;
 
     private void Start() {
         currentHealth = maxHealth;
+        g = Instantiate(healthPrefab, Vector3.zero, Quaternion.identity);
+        g.transform.SetParent(GameObject.Find("EnemyHealthCanvas").transform);
+    }
+
+    private void Update() {
+        print(g.transform.Find("Fill").GetComponent<Image>().fillAmount);
+    }
+
+    private void LateUpdate() {
+        g.transform.position = Camera.main.WorldToScreenPoint(transform.position + new Vector3(0, 1f, 0));
     }
 
     public void Damage(int dmg) {
         currentHealth -= dmg;
+        //g.transform.Find("Fill").GetComponent<Image>().fillAmount;
         if (currentHealth <= 0) {
             currentHealth = 0;
             Death();
