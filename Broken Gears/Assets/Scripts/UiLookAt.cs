5bf217e8020929fcf46fcdd32bb34c59d704d34c
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2019-09-18 13:25:52 +0200

    s

---
+++ Broken Gears/Assets/Scripts/UiLookAt.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiLookAt : MonoBehaviour {
+
+    public static Transform transformer;
+
+    private void Awake() {
+        transformer = GetComponent<Transform>();
+    }
+
+}
