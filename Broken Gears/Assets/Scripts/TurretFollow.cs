745cc445059a51a74afbd6226862dc92835528d3
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2019-09-19 15:39:23 +0200

    weap

--- Broken Gears/Assets/Scripts/TurretFollow.cs
+++ Broken Gears/Assets/Scripts/TurretFollow.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TurretFollow : MonoBehaviour {
-    public Transform enemy;
-    public GameObject gun;
     public GameObject gunBarrel;
+    public GameObject weap;
+    Weapon weapon;
     Transform t;
     Vector3 v;
+    public float turnSpeed;
+
+    private void Start() {
+        weapon = GetComponentInParent<Weapon>();   
+    }
     void Update() {
-        transform.LookAt(enemy);
-        gun.transform.LookAt(enemy);
-        Debug.DrawRay(transform.position, transform.forward, Color.red * 1000);
+        Vector3 dir = weapon.target.position - weap.transform.position;
+        Quaternion armLookRotation = Quaternion.LookRotation(dir);
+        Quaternion gunLookRotation = Quaternion.LookRotation(dir);
+        Vector3 armRotation = Quaternion.Lerp(transform.rotation, armLookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        Vector3 gunRotation = Quaternion.Lerp(weap.transform.rotation, gunLookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        transform.rotation = Quaternion.Euler(0f, armRotation.y, 0f);
+        weap.transform.localRotation = Quaternion.Euler(0f, 0f, gunRotation.z);
         Debug.DrawRay(gunBarrel.transform.position, gunBarrel.transform.forward, Color.blue * 1000);
     }
 }
---
+++ Broken Gears/Assets/Scripts/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    public Transform target;
+
+    public GameObject barrel;
+    public float range;
+
+    private void Start() {
+        UpdateTarget();
+    }
+
+    void UpdateTarget() {
+        GameObject[] targets = GameObject.FindGameObjectsWithTag("EnemyTarget");
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
