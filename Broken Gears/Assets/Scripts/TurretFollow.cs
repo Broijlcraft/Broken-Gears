d9947b701e095f5c3a814dfd782ef304070568ca
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2019-09-19 14:26:12 +0200

    Toeret

---
+++ Broken Gears/Assets/Scripts/TurretFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretFollow : MonoBehaviour {
+    public Transform enemy;
+    public GameObject gun;
+    public GameObject gunBarrel;
+    Transform t;
+    Vector3 v;
+    void Update() {
+        transform.LookAt(enemy);
+        gun.transform.LookAt(enemy);
+        Debug.DrawRay(transform.position, transform.forward, Color.red * 1000);
+        Debug.DrawRay(gunBarrel.transform.position, gunBarrel.transform.forward, Color.blue * 1000);
+    }
+}
