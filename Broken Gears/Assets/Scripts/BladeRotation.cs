2478db28919dc28b9b779685837823d648bd0a3e
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2019-09-25 14:29:24 +0200

    melee

---
+++ Broken Gears/Assets/Scripts/BladeRotation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BladeRotation : MonoBehaviour {
+    public Vector3 rot;
+    public float divider;
+    public float speed;
+    float actualSpeed;
+    Weapon weapon;
+
+    private void Start() {
+        weapon = GetComponentInParent<Weapon>();
+    }
+
+    private void Update() {
+        if (weapon.armTarget != weapon.defaultArmTarget) {
+            if (actualSpeed < speed) {
+                actualSpeed += speed / divider;
+            }
+        } else {
+            if (actualSpeed > 0) {
+                actualSpeed -= speed / divider;
+            } else {
+                actualSpeed = 0;
+            }
+        }
+        transform.Rotate(rot * actualSpeed);
+    }
+}
