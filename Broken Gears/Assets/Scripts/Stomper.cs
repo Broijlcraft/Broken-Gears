abcaf5c0c233246e268bd0fdc5c2e62b14f6e45b
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2019-10-02 08:29:16 +0200

    mmm

--- Broken Gears/Assets/Scripts/Stomper.cs
+++ Broken Gears/Assets/Scripts/Stomper.cs
@@ -3,12 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Stomper : MonoBehaviour {
-    public Vector3 down;
+    public float attackSpeed;
+    public float decentSpeed;
+    public float ascentSpeed;
+    float actualSpeed;
+    public Transform target;
+    public Transform targetPointUp;
+    public Transform targetPointDown;
+    public float maxDistance;
 
-    void Stomp() {
-        Vector3 armDir = transform.position - down;
-        Quaternion armLookRotation = Quaternion.LookRotation(armDir);
-        //Vector3 armRotation = Quaternion.Lerp(transform.rotation, armLookRotation, Time.deltaTime * weapon.turnSpeed).eulerAngles;
-        //transform.rotation = Quaternion.Euler(0f, armRotation.y, 0f);
+    private void Start() {
+        actualSpeed = decentSpeed;
+        target = targetPointDown;
+    }
+
+    private void Update() {
+        Vector3 direction = abs(targetPointDown.localPosition, transform.localPosition);
+        print(direction);
+        Vector3 test = target.localPosition - transform.localPosition;
+        if (direction.z < maxDistance && direction.x < maxDistance) {
+            ChangeDirection();
+        }
+        transform.Translate(test.normalized * actualSpeed * Time.deltaTime);
+    }
+
+    void ChangeDirection() {
+        transform.localPosition = target.localPosition;
+        if (target == targetPointDown) {
+            target = targetPointUp;
+            actualSpeed = ascentSpeed;
+            print("Yeah");
+        } else {
+            //target = targetPointDown;
+            //actualSpeed = decentSpeed;
+        }
+    }
+
+    Vector3 abs(Vector3 v, Vector3 vA) {
+        Vector3 vB = v - vA;
+        float x = Mathf.Abs(vB.x);
+        float y = Mathf.Abs(vB.y);
+        float z = Mathf.Abs(vB.z);
+        return new Vector3(x, y, z);
     }
 }
