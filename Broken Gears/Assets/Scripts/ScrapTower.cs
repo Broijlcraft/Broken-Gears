802d10c80d3b4ff55858bf5bc9342b4f563dc494
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2019-10-18 15:21:32 +0200

    fab

---
+++ Broken Gears/Assets/Scripts/ScrapTower.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrapTower : MonoBehaviour {
+
+    public List<GameObject> activeEffects = new List<GameObject>();
+
+    public Vector4 enabledColor;
+
+}
