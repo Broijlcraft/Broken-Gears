8ef10c46cd3181a28cca9a6cd114f252065c1859
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2019-10-18 14:30:59 +0200

    uifab

---
+++ Broken Gears/Assets/Scripts/Sfx.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "SFX", menuName = "SFX")]
+public class Sfx : ScriptableObject {
+
+    public GameObject soundFab;
+    public AudioClip audioKlip;
+    public AudioSource source;
+
+}
