b7e7625e2025c71e516153e4995f53c06a97c95d
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-08-06 17:29:43 +0200

    Cleanup

--- Broken Gears/Assets/Code/New/StatusFX/FreezeFX.cs
+++ Broken Gears/Assets/Code/New/StatusFX/FreezeFX.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class FreezeFX : StatusFX {
+﻿public class FreezeFX : StatusFX {
 
     private void Update() {
         if (shouldFX) {
--- Broken Gears/Assets/Code/New/Towers/TowerManager.cs
+++ Broken Gears/Assets/Code/New/Towers/TowerManager.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class TowerManager : MonoBehaviour {
     public static TowerManager tm_Single;
     public LayerMask layersToIgnore = (1 << 8);
+
+    [Header("HideInInspector")]
+    public Tower selectedTower;
 
     private void Awake() {
         tm_Single = this;
